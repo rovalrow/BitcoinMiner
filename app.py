dfbf37c89cb32a3c0cb5c43da716835576522c5b
Ezjq <kokieunz@gmail.com>
2025-05-26 14:00:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,5 @@ def logout():
     session.clear()
     return redirect("/")
 
-# Do not use this for production
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
